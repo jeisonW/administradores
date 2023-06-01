814b361ca0afa7bf98f3f2d55a578ce8f9b7c698
shark503 <76966259+shark503@users.noreply.github.com>
2023-06-01 14:07:12 -0600

    Se modifico el js para que no cree conflicto

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,7 +1,18 @@
-var menu_btn = document.querySelector("#menu-btn")
-var sidebar = document.querySelector("#sidebar")
-var container = document.querySelector(".my-container")
-menu_btn.addEventListener("click", () => {
-    sidebar.classList.toggle("active-nav")
-    container.classList.toggle("active-cont")
-})
+window.addEventListener('load', function() {
+    var menu_btn = document.querySelector("#menu-btn");
+    var sidebar = document.querySelector("#sidebar");
+    var container = document.querySelector(".my-container");
+    
+    //esto es para que no cree conflicto si no encuentra el nombre de los contenedores
+    if (menu_btn && sidebar && container) {
+      menu_btn.addEventListener("click", function() {
+        sidebar.classList.toggle("active-nav");
+        container.classList.toggle("active-cont");
+      });
+  
+      // Activar el menú al cargar la página
+      sidebar.classList.add("active-nav");
+      container.classList.add("active-cont");
+    }
+  });
+  
